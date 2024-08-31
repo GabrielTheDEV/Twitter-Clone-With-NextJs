6a788bdbbdb9bd29e36b9da71214f78b041c9c03
TheDeveloper <gabriel4321hh@gmail.com>
2024-08-31 15:32:27 -0300

    header component was added to the PostPage component

--- app/postPage/[slug]/page.tsx
+++ app/postPage/[slug]/page.tsx
@@ -6,8 +6,14 @@ export default function PostPage({params}:{params:{slug:string}}){
     return(
         <PageComponent>
             <Header border={true}>
+                <div className="flex items-center justify-center gap-2 p-2">
                 <BackButton/>
+                    <div>
+                        <span className="text-xl font-medium">Post</span>
+                    </div>
+                </div>
             </Header>
+            
             <div>{params.slug}</div>
         </PageComponent>
     )
