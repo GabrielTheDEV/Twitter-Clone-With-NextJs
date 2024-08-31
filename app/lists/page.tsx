94d4c5026c89f1b46be4d906ed77497047cbcdd7
TheDeveloper <gabriel4321hh@gmail.com>
2024-08-31 15:34:15 -0300

    header was added to the ListPage

--- app/lists/page.tsx
+++ app/lists/page.tsx
@@ -1,8 +1,15 @@
 import { PageComponent } from "@/components/pageComponent";
+import { BackButton } from "@/components/ui/backButton";
+import { Header } from "@/components/ui/header";
 
 export default function ListsPage(){
     return (
         <PageComponent>
+            <Header border={true}>
+                <div className="p-2">
+                    <BackButton/>
+                </div>
+            </Header>
             <h1>Hello Lists page</h1>
         </PageComponent>
     )
